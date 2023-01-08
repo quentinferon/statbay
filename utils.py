f32b39ca6f25b091606c470f979e88c546efe301
quentinferon <57185645+quentinferon@users.noreply.github.com>
2023-01-08 15:24:20 +0100

    UPDATE : utils.py

--- utils.py
+++ utils.py
@@ -13,6 +13,16 @@ def generate_mu_psi():
     psi = np.random.normal(560,476)
     return (mu,psi)
 
+def sample_z(P_y,Z,n,K_bis,N,alpha,m):
+    Z_bis = Z[:n]+Z[n+1:]
+    for k in range(len(P_y)):
+        if k <= K_bis:
+            P_y[k]*= Z_bis.count(k)/(alpha + N - 1)
+        else :
+            P_y[k] *= (alpha/m)/(alpha + N - 1)
+
+
+
 def InferDPnSSM(
     Y : pd.DataFrame,
     alpha : float, 
@@ -21,14 +31,17 @@ def InferDPnSSM(
     Z : List,
     Theta : List[tuple]
     ):
+    N = len(Y)
     for i in range(1,I):
         Z_i,Theta_i = Z,Theta
-        for n in range(len(Y)):
+        for n in range(N):
             K_bis = count_other_cluster(Z_i,n)
+            P_y = []
             for k in range(K_bis+m):
                 if k <= K_bis:
-                    P_y = cSMC(Y[n],Theta_i[k])
+                    P_y.append(cSMC(Y[n],Theta_i[k]))
                 else : 
                     Theta_i.append(generate_mu_psi())
-                    P_y = cSMC(Y[n],Theta_i[k])
+                    P_y.append(cSMC(Y[n],Theta_i[k]))
+            Z_i[n] = sample_z(P_y,Z_i,n,K_bis,N,alpha,m)
                 
